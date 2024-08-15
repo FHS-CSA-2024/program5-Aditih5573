dee6d89ceb0e829eac9277a477c0711d42919e77
Aditi <109041004+Aditih5573@users.noreply.github.com>
2024-08-15 12:32:26 -0700

    Update Program5.java

--- Program5.java
+++ Program5.java
@@ -7,3 +7,24 @@
 
 
 */
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Try programiz.pro");
+        int royalemMiles = 286;
+        int koopaMiles = 412;
+        int pipeMiles = 361;
+        int badwagonMiles = 161;
+       
+        int royaleGallons = 9;
+        int koopaGallons = 40
+        int pipeGallons = 18;
+        int badwagonGallons = 11;
+        
+        double royaleAverage = royaleMiles/royaleGallons;
+        System.out.print.ln("Royale averaged" + royaleAverage)
+        
+    }
+}
